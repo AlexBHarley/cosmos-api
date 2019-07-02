7d67dc40dad5cd078042adac925830b68a68d6bb
Alex Harley <alexbruceharley@gmail.com>
2019-07-02 15:20:19 +0200

    chore: publishing temp branch

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,36 @@
 const config = {
   devtool: "cheap-source-map",
-  entry: ['babel-polyfill', './src/index.js'],
+  entry: ["babel-polyfill", "./src/index.js"],
+  mode: "development",
   output: {
-    path: __dirname + '/lib',
-    filename: 'cosmos.js',
-    library: 'cosmos-api',
-    libraryTarget: 'umd',
+    path: __dirname + "/lib",
+    filename: "cosmos.js",
+    library: "cosmos-api",
+    libraryTarget: "umd",
     umdNamedDefine: true,
-    globalObject: 'typeof self !== \'undefined\' ? self : this',
+    globalObject: "typeof self !== 'undefined' ? self : this"
   },
   module: {
     rules: [
       {
-        loader: 'babel-loader',
+        loader: "babel-loader",
         test: /\.js$/,
         exclude: /node_modules/,
         query: {
-          presets: ['@babel/preset-env']
+          presets: ["@babel/preset-env"]
         }
       }
     ]
   },
   externals: [
     {
-      'isomorphic-fetch': {
-        root: 'isomorphic-fetch',
-        commonjs2: 'isomorphic-fetch',
-        commonjs: 'isomorphic-fetch',
-        amd: 'isomorphic-fetch'
+      "isomorphic-fetch": {
+        root: "isomorphic-fetch",
+        commonjs2: "isomorphic-fetch",
+        commonjs: "isomorphic-fetch",
+        amd: "isomorphic-fetch"
       }
     }
   ]
-}
+};
 module.exports = config;
